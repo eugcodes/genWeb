790c7546f350827d54b66b4799dc0cba8c2991d7
eugisbreakingthingsagain <17417539+eugisbreakingthingsagain@users.noreply.github.com>
2023-07-18 19:20:27 -0400

    Added mini roadmap

--- genWeb.py
+++ genWeb.py
@@ -7,6 +7,7 @@ import os
 
 openai.api_key = st.secrets["apiSecret"]
 
+# Create initial pearl
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
@@ -17,26 +18,20 @@ response = openai.ChatCompletion.create(
   max_tokens=128,
 )
 
-# create and initiatlize app hit counter
+# Write text
+st.title("Welcome, digital wanderer!")
+st.write("This is an experimental sandbox.")
+st.write("\nDaily nugget of wisdom:")
+
+# Write first pearl
+st.write(response["choices"][0]["message"]["content"])
+
+# create and initiatize app hit counter
 if 'hitCount' not in st.session_state:
     st.session_state.hitCount = 0
 
 # increment app hit count
 #st.session_state.hitCount += 1
-
-#implement some kind of website hit counter
-#https://www.geeksforgeeks.org/python-count-number-of-hits-on-a-particular-url-using-flask/
-
-# Create a Streamlit app
-st.title("Welcome, digital wanderer!")
-st.write("This is an experimental sandbox.")
-st.write("\nDaily nugget of wisdom:")
-
-# Generate a random text
-st.write(response["choices"][0]["message"]["content"])
-
-# generate a new response every 24 hours
-#https://discuss.streamlit.io/t/how-to-run-a-function-every-24-hours/1809/2
 
 increment = st.button('Please sir, I want some more!')
 
@@ -45,6 +40,10 @@ if increment:
 
 st.write("\n", st.session_state.hitCount,"served")
 
+
+# improvements
+# 1. improve performance. Cache results in queue?
+# 2. perist counter across all sessions. e.g. write/retrieve count to static file?
 
 
 
